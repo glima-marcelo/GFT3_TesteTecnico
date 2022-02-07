0c21ce322491aa34856f740cadfda5873ed7b8e3
glima-marcelo <glima.marcelo@gmail.com>
2022-02-07 16:47:28 -0300

    Primeiro commit, desafio 1

---
+++ Teste1/ContarLetra/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContarLetra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> frase = new List<String>();
+
+            frase.Add("Na minha máquina funciona");
+            frase.Add("Eu posso pular o teste");
+            frase.Add("Isso não deve demorar");
+            
+            foreach (string str in frase)
+            {
+                Console.WriteLine(str);
+                string[] ultimaPalavra = str.Split(' ');
+                Console.WriteLine(ultimaPalavra[ultimaPalavra.Length - 1].Length);
+            }            
+        }
+    }
+}
